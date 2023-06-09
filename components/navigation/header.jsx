dec175c535e2e572f512e311845cdbfd96a34d93
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-06-09 14:45:13 +0530

    Fixed following issues1. fixed the header menu for mobile view 2. updated the selected prefernce, noe you can see selected preference after the model is closed

--- components/navigation/header.jsx
+++ components/navigation/header.jsx
@@ -32,33 +32,26 @@ export default function Header(){
                         fill="none"
                         viewBox="0 0 24 24"
                     >
-                        {open ? (
-                        <>
-                            <path
-                            className="hidden"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18L18 6M6 6l12 12"
-                            ></path>
-                        </>
-                        ) : (
-                        <>
-                            <path
-                            className="inline-flex"
+                         <path
+                            className={open ? 'hidden' : 'inline-flex'}
                             strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth="2"
                             d="M4 6h16M4 12h16M4 18h16"
-                            ></path>
-                        </>
-                        )}
+                        ></path>
+                        <path
+                            className={!open ? 'hidden' : 'inline-flex'}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                        ></path>
                     </svg>
                 </button>
                 
             </div>
             
-            <nav className={`flex-col items-center flex-grow ${open ? '' : 'hidden'} md:pb-0 md:flex md:justify-end md:flex-row`}>
+            <nav className={`flex-col items-center flex-grow ${open ? 'flex' : 'hidden'} md:pb-0 md:flex md:justify-end md:flex-row`}>
                 <a className="px-2 lg:px-6 py-2 md:px-3 text-sm font-medium text-black hover:text-accent-400 lg:ml-auto" href="/#features">
                     Tools
                 </a>
@@ -80,7 +73,7 @@ export default function Header(){
             
         </div>
         <div>
-            <hr class="w-full h-0.5 border-t-0 bg-slate-50 opacity-200 dark:opacity-200" />
+            <hr class="w-full h-0.5 border-t-0 mt-2 bg-slate-50 opacity-200 dark:opacity-200" />
         </div>
         </div>
 
